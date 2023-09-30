c0da32ac8b8378a758bce47d43d9e8928816922c
Drei4 <antipin-lukinf@mail.ru>
2023-09-30 21:02:38 +0300

    register.html

--- test_flask/users/routes.py
+++ test_flask/users/routes.py
@@ -7,3 +7,22 @@ from test_flask.users.forms import (RegistrationForm, LoginForm, UpdateAccountFo
 from test_flask.users.utils import save_picture
 
 users = Blueprint('users', __name__)
+
+
+@users.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.\
+            generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Ваша учетная запись была создана!'
+              ' Теперь вы можете войти в систему', 'success')
+        return redirect(url_for('users.login'))
+    return render_template('register.html', title='Register', form=form)
+
