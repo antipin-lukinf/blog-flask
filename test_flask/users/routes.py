2dde7b8205dc115ad99b1e0114a6530ed7b1f413
Drei4 <antipin-lukinf@mail.ru>
2023-10-03 21:06:37 +0300

    profile

--- test_flask/users/routes.py
+++ test_flask/users/routes.py
@@ -44,3 +44,32 @@ def login():
             flash('Войти не удалось. Пожалуйста, '
                   'проверьте электронную почту и пароль', 'внимание')
     return render_template('login.html', title='Авторизация', form=form)
+
+
+@users.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Ваш аккаунт был обновлен!', 'success')
+        return redirect(url_for('users.account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        page = request.args.get('page', 1, type=int)
+        user = User.query.filter_by(username=
+                                    form.username.data).first_or_404()
+        posts = Post.query.filter_by(author=user) \
+            .order_by(Post.date_posted.desc()) \
+            .paginate(page=page, per_page=5)
+    image_file = url_for('static', filename='profile_pics/' +
+                                            current_user.image_file)
+    return render_template('account.html', title='Аккаунт',
+                           image_file=image_file, form=form, posts=posts,
+                           user=user)
