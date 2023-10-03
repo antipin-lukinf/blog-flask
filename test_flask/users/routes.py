c3f5bcecc26e6096e027e1c86b7a9a34fa5081fd
Drei4 <antipin-lukinf@mail.ru>
2023-10-03 20:52:04 +0300

    complit autorization

--- test_flask/users/routes.py
+++ test_flask/users/routes.py
@@ -15,7 +15,7 @@ def register():
         return redirect(url_for('main.home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.\
+        hashed_password = bcrypt. \
             generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data,
                     email=form.email.data, password=hashed_password)
@@ -23,6 +23,24 @@ def register():
         db.session.commit()
         flash('Ваша учетная запись была создана!'
               ' Теперь вы можете войти в систему', 'success')
-        return redirect(url_for('users.home'))
+        return redirect(url_for('main.home'))
     return render_template('register.html', title='Register', form=form)
 
+
+@users.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,
+                                               form.password.data):
+            login_user(user, remember=form.remember.data)
+
+            return redirect(url_for('main.home'))
+        else:
+            flash('Войти не удалось. Пожалуйста, '
+                  'проверьте электронную почту и пароль', 'внимание')
+    return render_template('login.html', title='Авторизация', form=form)
