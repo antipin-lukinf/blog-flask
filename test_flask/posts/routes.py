a0ebd99e47c6bab2fbcceea81d975c86563e7c20
Drei4 <antipin-lukinf@mail.ru>
2023-10-13 20:58:58 +0300

    del upate post

--- test_flask/posts/routes.py
+++ test_flask/posts/routes.py
@@ -36,3 +36,35 @@ def new_post():
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
+
+
+@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])  # обновление поста
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Ваш пост обновлен!', 'success')
+        return redirect(url_for('posts.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Обновление поста',
+                           form=form, legend='Обновление поста')
+
+
+@posts.route("/post/<int:post_id>/delete", methods=['POST'])  # удаление поста
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Ваш пост был удален!', 'success')
+    return redirect(url_for('posts.allpost'))
